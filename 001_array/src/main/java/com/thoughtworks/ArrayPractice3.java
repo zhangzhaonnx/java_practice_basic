d75ca0d661b0a3063198ced324de423601781c96
nonpool <nonpool@163.com>
2020-02-03 00:39:20 +0800

    添加数组练习3 4

---
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -0,0 +1,12 @@
+package com.thoughtworks;
+
+public class ArrayPractice3 {
+
+    /**
+     * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
+     */
+    public static int[] filterZero(int[] array) {
+
+        return null;
+    }
+}
---
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice4.java
@@ -0,0 +1,13 @@
+package com.thoughtworks;
+
+public class ArrayPractice4 {
+
+    /**
+     * 完成本方法实现功能: 将一个数字插入到给定的已经排好序的数组中，要求插入以后数组依然是有序的
+     */
+    public static int[] insert(int number) {
+        int[] array = new int[]{1, 20, 50, 100};
+
+        return null;
+    }
+}
