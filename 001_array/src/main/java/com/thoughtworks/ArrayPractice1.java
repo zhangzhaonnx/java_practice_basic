aacd19c8348d6776c65f814c1b50fa1af7277dd0
nonpool <nonpool@163.com>
2020-02-03 00:20:12 +0800

    添加打印格式

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice1.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice1.java
@@ -7,7 +7,7 @@ public class ArrayPractice1 {
     }
 
     /**
-     * 倒序打印给定数组
+     * 倒序打印给定数组,打印格式为: [3,2,1]
      */
     public static void printArrayReverse() {
         int[] array = new int[]{1, 2, 3};
