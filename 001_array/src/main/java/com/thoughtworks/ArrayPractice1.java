9d264777d6d14128b3cb42dd13e09383949e6b7e
nonpool <nonpool@163.com>
2020-02-03 00:18:10 +0800

    添加两道数组练习题

---
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice1.java
@@ -0,0 +1,16 @@
+package com.thoughtworks;
+
+public class ArrayPractice1 {
+
+    public static void main(String[] args) {
+        printArrayReverse();
+    }
+
+    /**
+     * 倒序打印给定数组
+     */
+    public static void printArrayReverse() {
+        int[] array = new int[]{1, 2, 3};
+
+    }
+}
---
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice2.java
@@ -0,0 +1,14 @@
+package com.thoughtworks;
+
+public class ArrayPractice2 {
+
+    /**
+     * 把给定数组中的最大值保存到数组中的第1个元素且原第一个数换到最大值的位置
+     */
+    public static int[] exchange() {
+        int[] array = new int[]{10, 8, 1, 7, 0, 20, 16, 19};
+
+
+        return null;
+    }
+}
